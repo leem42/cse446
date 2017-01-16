de6ad4edaead76502fa02df12568754e770c433b
michael lee <keymikey54@yahoo.com>
2017-01-16 15:14:45 -0800

    updates to code

--- src/hw1/code/Crime_predictor.py
+++ src/hw1/code/Crime_predictor.py
@@ -2,30 +2,53 @@
 Created on Jan 16, 2017
 
 @author: leem42
+
+                 
 '''
 
-import pandas as pd 
-import sys, os
 
-
+from __future__ import division
 from json.encoder import INFINITY
+
+import pandas as pd 
+import sys, os, numpy, matplotlib.pyplot, pickle
+
+
 
 def main(args):
     
-    
-    lamda = args[1]
+    lamda = 600
     response = args[2]
     matrix = args[3]
-    initial_weights = args[4]
+    weights = numpy.random.normal(size=96)
     output = 0
 
     df_train = pd.read_table('crime-test.txt')
     df_test = pd.read_table('crime-train.txt')
-
-
+    
     diff = INFINITY
-    
-    
+    while( diff > 10e-6):
+        for j in range(len(df_train.values)):
+            # accessing feature j of design matrix df_train
+            a_j = 2 * sum(float(x) for x in df_train[df_train.columns[j]].values)
+            c_j = 0
+            ## at column j, accessing each value (ie. the ith value)
+            for value in df_train[df_train.columns[j]].values:
+                ### multiple our jth-weight times the data point at i
+                mid = (weights[j] * value) - (numpy.transpose(weights) * df_train.iloc[0,0:]) 
+                mid = mid  + weights[j]*value ### right multiplier
+                c_j+= (value * mid)                
+            c_j*=2
+            weights[j] = soft(c_j / a_j, lamda / a_j)
+            diff = max(diff,abs(weights[j]))
+            
+    print weights
+    pickle.dumps(weights, "weights.obj")
+
+
+def soft(a,b):    
+    return numpy.sign(a)*(abs(a) - b)
+        
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main(sys.argv) 
---
+++ src/hw1/code/Graphs.py
@@ -0,0 +1,35 @@
+'''
+Created on Jan 16, 2017
+
+@author: leem42
+'''
+
+import matplotlib.pyplot
+
+
+w
+
+
+# 2. (10 points) The regularization paths (in one plot) for the coefficients for input variables agePct12t29,
+# pctWSocSec, PctKids2Par, PctIlleg, and HousVacant — use log(λ) instead of λ.
+
+
+# 3. (4 points) A plot of log(λ) against the squared error in the training data.
+
+
+
+# 4. (4 points) A plot of log(λ) against the squared error in the test data.
+
+
+
+# 5. (3 points) A plot of λ against the number of nonzero coefficients.
+
+
+
+# 6. (2 points) A brief commentary on the task of selecting λ
+
+
+
+
+if __name__ == '__main__':
+    pass
