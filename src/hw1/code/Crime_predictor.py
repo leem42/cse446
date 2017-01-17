58e28a775bbb6f869ccbc14a4005f8f9cf3ec354
michael lee <keymikey54@yahoo.com>
2017-01-17 10:53:48 -0800

    minor updates

--- src/hw1/code/Crime_predictor.py
+++ src/hw1/code/Crime_predictor.py
@@ -20,24 +20,25 @@ def main(args):
     lamda = 600
  #  response = args[2]
  #  matrix = args[3]
-    weights = numpy.random.normal(size=96)
+    weights = numpy.array(numpy.random.normal(size=96), dtype=float)
     output = 0
 
     df_train = pd.read_table('crime-test.txt')
-    df_test = pd.read_table('crime-train.txt')
     
     index = 0
     
-    diff = INFINITY * -1
-    while( index < 1 ):
+    diff = 10e-5
+    while( diff > 10e-6 ):
+        diff = None
+        print weights
         for j in range(len(df_train.columns)):
             # accessing feature j of design matrix df_train
-            a_j = 2 * sum(float(x) for x in df_train[df_train.columns[j]].values)
+            a_j = 2 * numpy.sum(df_train[df_train.columns[j]].values)
             c_j = 0
             ## at column j, accessing each value (ie. the ith value)
             for i,value in enumerate(df_train[df_train.columns[j]].values):
                 ### multiple our jth-weight times the data point at i
-                y_i =  sum(numpy.transpose(weights) * df_train.iloc[i,0:]) 
+                y_i =  sum(numpy.transpose(weights) *numpy.array(df_train.iloc[i,0:])) 
                 sub_j = numpy.transpose(numpy.delete(weights,j)) * numpy.delete(numpy.array(df_train.iloc[i,0:]),j)   
                 total = value * ( y_i - sub_j + weights[j]*value)     
                 c_j+= value
@@ -45,16 +46,17 @@ def main(args):
             weight_old= weights[j]
             weights[j] = soft(c_j / a_j, lamda / a_j)
             diff = max(diff,abs(weights[j] - weight_old))
-        if(diff < 10e-6):
-            diff = INFINITY * -1
-            index+=1
-            lamda = lamda / 2
-            print weights
-            print 'agePct12t29 = ' + str(weights[df_train.columns.get_loc('agePct12t29')])
-            print 'pctWSocSec = ' + str(weights[df_train.columns.get_loc('pctWSocSec')])
-            print 'PctKids2Par = ' + str(weights[df_train.columns.get_loc('PctKids2Par')])
-            print 'PctIlleg = ' + str(weights[df_train.columns.get_loc('PctIlleg')])
-            print 'HousVacant = ' + str(weights[df_train.columns.get_loc('HousVacant')])
+            print abs(weights[j] - weight_old)
+        print weights
+        print 'iteration ' + str(diff)
+
+     
+    print weights
+    print 'agePct12t29 = ' + str(weights[df_train.columns.get_loc('agePct12t29')])
+    print 'pctWSocSec = ' + str(weights[df_train.columns.get_loc('pctWSocSec')])
+    print 'PctKids2Par = ' + str(weights[df_train.columns.get_loc('PctKids2Par')])
+    print 'PctIlleg = ' + str(weights[df_train.columns.get_loc('PctIlleg')])
+    print 'HousVacant = ' + str(weights[df_train.columns.get_loc('HousVacant')])
 
 #    pickle.dump(weights, open("weights.obj","wb"))
 
