9fcb3fad1ff989306bd61b0a644f64c5a571080a
michael lee <keymikey54@yahoo.com>
2017-01-16 21:08:48 -0800

    made changes to code

--- src/hw1/code/Crime_predictor.py
+++ src/hw1/code/Crime_predictor.py
@@ -18,32 +18,45 @@ import sys, os, numpy, matplotlib.pyplot, pickle
 def main(args):
     
     lamda = 600
-    response = args[2]
-    matrix = args[3]
+ #  response = args[2]
+ #  matrix = args[3]
     weights = numpy.random.normal(size=96)
     output = 0
 
     df_train = pd.read_table('crime-test.txt')
     df_test = pd.read_table('crime-train.txt')
     
-    diff = INFINITY
-    while( diff > 10e-6):
-        for j in range(len(df_train.values)):
+    index = 0
+    
+    diff = INFINITY * -1
+    while( index < 1 ):
+        for j in range(len(df_train.columns)):
             # accessing feature j of design matrix df_train
             a_j = 2 * sum(float(x) for x in df_train[df_train.columns[j]].values)
             c_j = 0
             ## at column j, accessing each value (ie. the ith value)
-            for value in df_train[df_train.columns[j]].values:
+            for i,value in enumerate(df_train[df_train.columns[j]].values):
                 ### multiple our jth-weight times the data point at i
-                mid = (weights[j] * value) - (numpy.transpose(weights) * df_train.iloc[0,0:]) 
-                mid = mid  + weights[j]*value ### right multiplier
-                c_j+= (value * mid)                
+                y_i =  sum(numpy.transpose(weights) * df_train.iloc[i,0:]) 
+                sub_j = numpy.transpose(numpy.delete(weights,j)) * numpy.delete(numpy.array(df_train.iloc[i,0:]),j)   
+                total = value * ( y_i - sub_j + weights[j]*value)     
+                c_j+= value
             c_j*=2
+            weight_old= weights[j]
             weights[j] = soft(c_j / a_j, lamda / a_j)
-            diff = max(diff,abs(weights[j]))
-            
-    print weights
-    pickle.dumps(weights, "weights.obj")
+            diff = max(diff,abs(weights[j] - weight_old))
+        if(diff < 10e-6):
+            diff = INFINITY * -1
+            index+=1
+            lamda = lamda / 2
+            print weights
+            print 'agePct12t29 = ' + str(weights[df_train.columns.get_loc('agePct12t29')])
+            print 'pctWSocSec = ' + str(weights[df_train.columns.get_loc('pctWSocSec')])
+            print 'PctKids2Par = ' + str(weights[df_train.columns.get_loc('PctKids2Par')])
+            print 'PctIlleg = ' + str(weights[df_train.columns.get_loc('PctIlleg')])
+            print 'HousVacant = ' + str(weights[df_train.columns.get_loc('HousVacant')])
+
+#    pickle.dump(weights, open("weights.obj","wb"))
 
 
 def soft(a,b):    
--- src/hw1/code/Graphs.py
+++ src/hw1/code/Graphs.py
@@ -4,29 +4,26 @@ Created on Jan 16, 2017
 @author: leem42
 '''
 
-import matplotlib.pyplot
+import matplotlib.pyplot, pickle
+import pandas as pd
 
 
-w
+estimator = pickle.load( open("weights.obj","rb"))
 
+df_train = pd.read_table('crime-test.txt')
+df_test = pd.read_table('crime-train.txt')
 
 # 2. (10 points) The regularization paths (in one plot) for the coefficients for input variables agePct12t29,
-# pctWSocSec, PctKids2Par, PctIlleg, and HousVacant — use log(λ) instead of λ.
+# pctWSocSec, PctKids2Par, PctIlleg, and HousVacant — use log(lamda) instead of lamda.
+
+# 3. (4 points) A plot of log(lamda) against the squared error in the training data.
 
 
-# 3. (4 points) A plot of log(λ) against the squared error in the training data.
+# 4. (4 points) A plot of log(lamda) against the squared error in the test data.
 
+# 5. (3 points) A plot of lambda against the number of nonzero coefficients.
 
-
-# 4. (4 points) A plot of log(λ) against the squared error in the test data.
-
-
-
-# 5. (3 points) A plot of λ against the number of nonzero coefficients.
-
-
-
-# 6. (2 points) A brief commentary on the task of selecting λ
+# 6. (2 points) A brief commentary on the task of selecting lambda
 
 
 
