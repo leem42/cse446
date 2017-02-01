2d6da1f5790a8bbf2042241d7a3001747663d082
michael lee <keymikey54@yahoo.com>
2017-02-01 08:14:05 -0800

    implementation of average loss incorporated

--- src/hw2/sgd_linear_learner.py
+++ src/hw2/sgd_linear_learner.py
@@ -4,8 +4,10 @@ Created on Jan 30, 2017
 @author: leem42
 '''
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
 
 def main():
         
@@ -13,18 +15,26 @@ def main():
     train = pd.read_csv('HW2_training_data.csv').drop("label", axis=1)
     response = pd.read_csv('HW2_training_data.csv').iloc[0:,0]
     weights = np.zeros(len(train.columns))
+    avg_loss = 0
     
-    random_i = 0
-    
+    index = 1
     for iteration in range(10):
         for i in range(len(response)):
-                for j in range(len(train.columns)):
-                    random_i = np.random.randint(len(train.columns))
-                    x_ij = train.iloc[random_i,j]
-                    actual = response[random_i]
-                    x_i = train.iloc[random_i,:]
-                    partial_j = -2 * x_ij * (actual - np.dot(weights,x_i)) * eta
-                    weights[j] = partial_j
+            actual = response[i]
+            x_i = train.iloc[i,:]
+            for j in range(len(train.columns)):
+                x_ij = train.iloc[i,j]
+                partial_j = -2 * x_ij * (actual - np.dot(weights,x_i)) 
+                weights[j] = weights[j] + ( partial_j * eta)
+            avg_loss+= (np.dot(weights, x_i) - actual) ** 2
+            index+=1
+            #if(index >= 100 and index % 100 == 0):
+              #  print 'iteration ' + str(index)
+             #   print avg_loss / index
+            if(index % 500 == 0):
+                print np.linalg.norm(weights)
+
+    print index
     print weights
 
 if __name__ == '__main__':
--- src/hw2/sgd_logistic_learner.py
+++ src/hw2/sgd_logistic_learner.py
@@ -10,23 +10,30 @@ import pandas as pd
 import math
 
 def main():
-    eta= 0.8
+    eta= 0.00001
     train = pd.read_csv('HW2_training_data.csv').drop("label", axis=1)
     response = pd.read_csv('HW2_training_data.csv').iloc[0:,0]
     weights = np.zeros(len(train.columns))
-    
-    random_i = 0
+    avg_loss = 0
+    index = 1
     
     for iteration in range(10):
             for i in range(len(response)):
+                actual = response[i]
+                x_i = train.iloc[i,:]
+                weightsCopy = weights.copy()
                 for j in range(len(train.columns)):
-                    random_i = np.random.randint(len(train.columns))
-                    x_ij = train.iloc[random_i,j]
-                    actual = response[random_i]
-                    x_i = train.iloc[random_i,:]
-                    partial_j = weights[j] - x_ij * (indicator(actual) - prob_exp(weights, x_i))
-                    weights[j] = partial_j
-             
+                    x_ij = train.iloc[i,j]
+                    partial_j = weights[j] - eta * x_ij * (indicator(actual) - prob_exp(weights, x_i))
+                    weights[j] = partial_j 
+                avg_loss+= (np.dot(weightsCopy, x_i) - actual) ** 2
+                index+=1
+                if(index >= 100 and index % 100 == 0):
+                    print 'iteration ' + str(index)
+                    print avg_loss / index
+                if(index % 500 == 0):
+                    print np.linalg.norm(weights)
+    
     print weights
     
 def indicator(a):
@@ -37,7 +44,7 @@ def indicator(a):
 
 def prob_exp(a,b):
     dot = np.dot(a.T,b) * -1
-    denom = 1 + math.exp(dot)
+    denom = 1 + (math.e ** dot)
     return 1.0 / (denom)
         
 
