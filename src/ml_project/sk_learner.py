eaa67792a2f1b99b0da30fb4463f9039e137d6c9
michael lee <keymikey54@yahoo.com>
2017-02-25 15:46:25 -0800

    updates to code...

--- src/ml_project/sk_learner.py
+++ src/ml_project/sk_learner.py
@@ -12,6 +12,8 @@ import math, sys
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Ridge
 import pickle
+import re
+
 def main(args):
     args=[0,"0.0001","5"]
     if(len(args) != 3):
@@ -26,162 +28,197 @@ def main(args):
     eta= float(args[1])
     ITERATIONS = int(args[2])
     
-    #Get Data
-    df = pd.read_csv('polished_output.csv')
-    train, test = train_test_split(df, test_size = 0.2)
+#     #Get Data
+#     df = pd.read_csv('polished_output.csv')
+#     train, test = train_test_split(df, test_size = 0.2)
+#     
+#     train['bias'] = np.ones(len(train.iloc[:,0])) 
+#     response = train.iloc[:,2]
+#     train =  train.drop("normalized_count", axis=1)
+# #     train = train.drop("ParticipantBarcode",axis=1)
+# #     train = train.drop("original_gene_symbol",axis=1)      
+#       
+#     output = (train.iloc[:,10] == 0)
+#  
+#     stage_one = (train.iloc[:,10] == 'Stage I')
+#     train['stage_one'] = stage_one.astype(int)
+#      
+#     stage_oneA = (train.iloc[:,10] == 'Stage IA')
+#     train['stage_oneA'] = stage_oneA.astype(int)
+#      
+#     stage_two= (train.iloc[:,10] == 'Stage II')
+#     train['stage_two'] = stage_two.astype(int)
+#      
+#     stage_twoA= (train.iloc[:,10] == 'Stage IIA')
+#     train['stage_twoA'] = stage_twoA.astype(int)
+#  
+#     stage_twoB= (train.iloc[:,10] == 'Stage IIB')
+#     train['stage_twoB'] = stage_twoB.astype(int)
+#  
+#     stage_twoC= (train.iloc[:,10] == 'Stage IIC')
+#     train['stage_twoC'] = stage_twoC.astype(int)
+#  
+#     stage_three= (train.iloc[:,10] == 'Stage III')
+#     train['stage_three'] = stage_three.astype(int)
+#      
+#     stage_threeA= (train.iloc[:,10] == 'Stage IIIA')
+#     train['stage_threeA'] = stage_threeA.astype(int)
+#      
+#     stage_threeB= (train.iloc[:,10] == 'Stage IIIB')
+#     train['stage_threeB'] = stage_threeB.astype(int)
+#      
+#     stage_threeC= (train.iloc[:,10] == 'Stage IIIC')
+#     train['stage_threeC'] = stage_threeC.astype(int)
+#  
+#     stage_four= (train.iloc[:,10] == 'Stage IV')
+#     train['stage_four'] = stage_four.astype(int)
+#  
+#     stage_fourA= (train.iloc[:,10] == 'Stage IV')
+#     train['stage_fourA'] = stage_fourA.astype(int)
+#      
+#     train.iloc[:,10] = output.astype(int)
+#   
+#   
+#     test['bias'] = np.ones(len(test.iloc[:,0]))
+#     test_response = test.iloc[:,2]
+# #     test = test.drop("ParticipantBarcode",axis=1)
+#     test = test.drop("normalized_count",axis=1)
+# #     test = test.drop("original_gene_symbol",axis=1)
+#     output = (test.iloc[:,10] == 0)
+#  
+#     stage_one = (test.iloc[:,10] == 'Stage I')
+#     test['stage_one'] = stage_one.astype(int)
+#      
+#     stage_oneA = (test.iloc[:,10] == 'Stage IA')
+#     test['stage_oneA'] = stage_oneA.astype(int)
+#      
+#     stage_two= (test.iloc[:,10] == 'Stage II')
+#     test['stage_two'] = stage_two.astype(int)
+#      
+#     stage_twoA= (test.iloc[:,10] == 'Stage IIA')
+#     test['stage_twoA'] = stage_twoA.astype(int)
+#  
+#     stage_twoB= (test.iloc[:,10] == 'Stage IIB')
+#     test['stage_twoB'] = stage_twoB.astype(int)
+#  
+#     stage_twoC= (test.iloc[:,10] == 'Stage IIC')
+#     test['stage_twoC'] = stage_twoC.astype(int)
+#  
+#     stage_three= (test.iloc[:,10] == 'Stage III')
+#     test['stage_three'] = stage_three.astype(int)
+#      
+#     stage_threeA= (test.iloc[:,10] == 'Stage IIIA')
+#     test['stage_threeA'] = stage_threeA.astype(int)
+#      
+#     stage_threeB= (test.iloc[:,10] == 'Stage IIIB')
+#     test['stage_threeB'] = stage_threeB.astype(int)
+#      
+#     stage_threeC= (test.iloc[:,10] == 'Stage IIIC')
+#     test['stage_threeC'] = stage_threeC.astype(int)
+#  
+#     stage_four= (test.iloc[:,10] == 'Stage IV')
+#     test['stage_four'] = stage_four.astype(int)
+#  
+#     test['stage_fourA'] = (test.iloc[:,10] == 'Stage IV').astype(int)
+#  
+#     test.iloc[:,10] = output.astype(int)
+#     
+#     locations = pd.read_table('chromosome_location.txt')
+#     ####################################
+#     #    preprocess chromsome          #
+#     ####################################
+#     num_rows = len(locations.iloc[:,0])
+#     for row in range(num_rows):
+#         new_location = locations.iloc[row,0].split("chr")[1] + locations.iloc[row,3]
+#         locations.iloc[row,3] = new_location
+#     
+#     ####################################
+#     #Add Gene Location to each patient #
+#     ####################################
+#     samples = len(train.iloc[:,0])
+#     genes = pd.read_table('total_genes.txt')
+#     gene_locations= []
+#     for row in range(0,samples):
+#         gene = train.iloc[row,1]
+#         relative_loc = get_gene_relaitve(gene, genes)
+#         actual_location = get_actual_location(relative_loc, locations)
+#         chromosome = actual_location[0]
+#         start = actual_location[1]
+#         end = actual_location[2]
+#         info = [chromosome,start,end]
+#         gene_locations.append(info)
+#     train['locations'] = gene_locations
+#     
+#     samples = len(test.iloc[:,0])
+#     gene_locations_test = []
+#     for row in range(0,samples):
+#         gene = test.iloc[row,1]
+#         relative_loc = get_gene_relaitve(gene, genes)
+#         actual_location = get_actual_location(relative_loc, locations)
+#         chromosome = actual_location[0]
+#         start = actual_location[1]
+#         end = actual_location[2]
+#         info = [chromosome,start,end]
+#         gene_locations_test.append(info)
+#     test['locations'] = gene_locations_test
     
-    train['bias'] = np.ones(len(train.iloc[:,0])) 
-    response = train.iloc[:,2]
-    train =  train.drop("normalized_count", axis=1)
-#     train = train.drop("ParticipantBarcode",axis=1)
-#     train = train.drop("original_gene_symbol",axis=1)      
-      
-    output = (train.iloc[:,10] == 0)
- 
-    stage_one = (train.iloc[:,10] == 'Stage I')
-    train['stage_one'] = stage_one.astype(int)
-     
-    stage_oneA = (train.iloc[:,10] == 'Stage IA')
-    train['stage_oneA'] = stage_oneA.astype(int)
-     
-    stage_two= (train.iloc[:,10] == 'Stage II')
-    train['stage_two'] = stage_two.astype(int)
-     
-    stage_twoA= (train.iloc[:,10] == 'Stage IIA')
-    train['stage_twoA'] = stage_twoA.astype(int)
- 
-    stage_twoB= (train.iloc[:,10] == 'Stage IIB')
-    train['stage_twoB'] = stage_twoB.astype(int)
- 
-    stage_twoC= (train.iloc[:,10] == 'Stage IIC')
-    train['stage_twoC'] = stage_twoC.astype(int)
- 
-    stage_three= (train.iloc[:,10] == 'Stage III')
-    train['stage_three'] = stage_three.astype(int)
-     
-    stage_threeA= (train.iloc[:,10] == 'Stage IIIA')
-    train['stage_threeA'] = stage_threeA.astype(int)
-     
-    stage_threeB= (train.iloc[:,10] == 'Stage IIIB')
-    train['stage_threeB'] = stage_threeB.astype(int)
-     
-    stage_threeC= (train.iloc[:,10] == 'Stage IIIC')
-    train['stage_threeC'] = stage_threeC.astype(int)
- 
-    stage_four= (train.iloc[:,10] == 'Stage IV')
-    train['stage_four'] = stage_four.astype(int)
- 
-    stage_fourA= (train.iloc[:,10] == 'Stage IV')
-    train['stage_fourA'] = stage_fourA.astype(int)
-     
-    train.iloc[:,10] = output.astype(int)
-  
-  
-    test['bias'] = np.ones(len(test.iloc[:,0]))
-    test_response = test.iloc[:,2]
-#     test = test.drop("ParticipantBarcode",axis=1)
-    test = test.drop("normalized_count",axis=1)
-#     test = test.drop("original_gene_symbol",axis=1)
-    output = (test.iloc[:,10] == 0)
- 
-    stage_one = (test.iloc[:,10] == 'Stage I')
-    test['stage_one'] = stage_one.astype(int)
-     
-    stage_oneA = (test.iloc[:,10] == 'Stage IA')
-    test['stage_oneA'] = stage_oneA.astype(int)
-     
-    stage_two= (test.iloc[:,10] == 'Stage II')
-    test['stage_two'] = stage_two.astype(int)
-     
-    stage_twoA= (test.iloc[:,10] == 'Stage IIA')
-    test['stage_twoA'] = stage_twoA.astype(int)
- 
-    stage_twoB= (test.iloc[:,10] == 'Stage IIB')
-    test['stage_twoB'] = stage_twoB.astype(int)
- 
-    stage_twoC= (test.iloc[:,10] == 'Stage IIC')
-    test['stage_twoC'] = stage_twoC.astype(int)
- 
-    stage_three= (test.iloc[:,10] == 'Stage III')
-    test['stage_three'] = stage_three.astype(int)
-     
-    stage_threeA= (test.iloc[:,10] == 'Stage IIIA')
-    test['stage_threeA'] = stage_threeA.astype(int)
-     
-    stage_threeB= (test.iloc[:,10] == 'Stage IIIB')
-    test['stage_threeB'] = stage_threeB.astype(int)
-     
-    stage_threeC= (test.iloc[:,10] == 'Stage IIIC')
-    test['stage_threeC'] = stage_threeC.astype(int)
- 
-    stage_four= (test.iloc[:,10] == 'Stage IV')
-    test['stage_four'] = stage_four.astype(int)
- 
-    test['stage_fourA'] = (test.iloc[:,10] == 'Stage IV').astype(int)
- 
-    test.iloc[:,10] = output.astype(int)
+    #######################################
+    #    Save our data set                #
+    #######################################
+    train_file = open('train.obj', 'rb')
+    train = pickle.load(train_file)
     
-    locations = pd.read_table('chromosome_location.txt')
-    ####################################
-    #    preprocess chromsome          #
-    ####################################
-    num_rows = len(locations.iloc[:,0])
-    for row in range(num_rows):
-        new_location = locations.iloc[row,0].split("chr")[1] + locations.iloc[row,3]
-        locations.iloc[row,3] = new_location
+    test_file = open('test.obj', 'rb')
+    test = pickle.load(test_file)
     
-    ####################################
-    #Add Gene Location to each patient #
-    ####################################
-    samples = len(train.iloc[:,0])
-    genes = pd.read_table('total_genes.txt')
-    gene_locations= []
-    for row in range(0,samples):
-        gene = train.iloc[row,1]
-        relative_loc = get_gene_relaitve(gene, genes)
-        actual_location = get_actual_location(relative_loc, locations)
-        chromosome = actual_location[0]
-        start = actual_location[1]
-        end = actual_location[2]
-        info = [chromosome,start,end]
-        gene_locations.append(info)
-    train['locations'] = gene_locations
     
-    for row in range(0,samples):
-        gene = test.iloc[row,1]
-        relative_loc = get_gene_relaitve(gene, genes)
-        actual_location = get_actual_location(relative_loc, locations)
-        chromosome = actual_location[0]
-        start = actual_location[1]
-        end = actual_location[2]
-        info = [chromosome,start,end]
-        gene_locations.append(info)
-    test['locations'] = gene_locations
-    
+    print 'finished editing dictionaries'
     ###################################
     ##### Find Nearest Mutation     ##
     ##################################
-    mutations = pickle.load("patient_mutations.obj","wb")
+    pkl_file = open('patient_mutations.obj', 'rb')
+    mutations = pickle.load(pkl_file)
     new_mutations = []
+    samples = len(train.iloc[:,0])
+    print len(train.iloc[:,0])
+    missing = set()
+    not_missing = set()
     for row in range(0,samples):
         id = train.iloc[row,0]
-        info = train.iloc[row,10]
-        chromosome = info[0]
+        info = train.iloc[row,:]['locations']
+        chromosome = info[0].split('chr')[1]
         start = info[1]
         end = info[2]
-        nearest = get_nearest(id,chromosome,start,end,mutations)
-        new_mutations.append(nearest)
+        nearest = get_nearest(id,chromosome,start,end,mutations,train,row)
+        if(nearest != 'Nan'):
+            print nearest
+            not_missing.appened(id)
+            new_mutations.append(nearest)
+        else:
+            missing.add(id)
+            
+    missing = missing.symmetric_difference()
+    for value in missing:
+        train = train[train.ParticipantBarcode != value]       
+             
+    print len(new_mutations)   
+    print len(train.iloc[:,0])
+    print len(missing)
     train['nearest'] = new_mutations
     
     new_mutations = []
+    samples = len(test.iloc[:,0])
+    missing = set()
     for row in range(0,samples):
         id = test.iloc[row,0]
         info = test.iloc[row,10]
         chromosome = info[0]
         start = info[1]
         end = info[2]
-        nearest = get_nearest(id,chromosome,start,end,mutations)
-        new_mutations.append(nearest)
+        nearest = get_nearest(id,chromosome,start,end,mutations,test,row)
+        if(nearest != 'Nan'):
+            missing.add(id)
+            new_mutations.append(nearest)
     test['nearest'] = new_mutations
     
     clf7 = Ridge(alpha= 200)
@@ -210,31 +247,38 @@ def main(args):
     matplotlib.pyplot.show()
 
 def get_gene_relaitve(name,table):
-    try:
-         return table.iloc[table['symbol'][table['symbol'] == name].index[0],:]['location']
-    except:
-        print name
-        return ''
-    
+    if (name == 'MGC16121'):
+        return 'Xq26.3'
+    if(name == 'C20orf111'):
+        return '20q13.12'
+    if (name == 'FLJ39653'):
+        return '4p15.32'
+    if(name == 'LOC728392'):
+        return '17p13.2'
+    return table.iloc[table['symbol'][table['symbol'] == name].index[0],:]['location']
+  
 
+### returns the [chromosome, start, end] end position
 def get_actual_location(relative,table):
     out = ''
     copy = []
     if('-' in relative):
         split = relative.split("-")
-        start = split[0]
-        end = split[1]
-    try:
-       out = table.iloc[table.iloc[:,3][table.iloc[:,3] == relative].index[0],:]
-       copy = [out[0],out[1],out[2]]
+        head = split[0]
+        tail =  re.split('p|q',head)[0] + split[1]
+        start = table.iloc[table.iloc[:,3][table.iloc[:,3] == head].index[0],:]
+        end = table.iloc[table.iloc[:,3][table.iloc[:,3] == tail].index[0],:][2]
+        return [start[0],start[1],end]
+    else:
+        out = table.iloc[table.iloc[:,3][table.iloc[:,3] == relative].index[0],:]
+        return  [out[0],out[1],out[2]]
+
+def get_nearest(id,chromosome,start,end,mutations,table,row):
+    try:    
+        mid = np.array([(start + end) / 2.0] * len(mutations[id][chromosome]))
     except:
-        print relative
-        print 'failed'
-        return ['','','']
-    return copy
-def get_nearest(id,chromosome,start,end,mutations):
-    mid = np.ndarray([(start + end) / 2.0] * len(mutations[id][chromosome]))
-    list_mutations = np.ndarray(sorted(mutations[id][chromosome]))
+        return 'Nan'
+    list_mutations = np.array(sorted(mutations[id][chromosome]))
     shortest = np.min(mid - list_mutations)
     return shortest
     
